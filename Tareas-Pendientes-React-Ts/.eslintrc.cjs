4dc31bff2540fda23ce188cebaa73f0aec68130d
Javi <javi.dev.79@gmail.com>
2024-05-21 21:21:40 +0100

    Proyecto final de lista de tareas con react y typescript

---
+++ Tareas-Pendientes-React-Ts/.eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    "prettier/prettier": "off",
+    "react/jsx-one-expression-per-line": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
